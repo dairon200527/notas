da1b21cf3c57566a0a6aaf5734b6f236435a74e3
Leydy Diaz <samueldiazborda55555@gmail.com>
2026-04-08 07:19:36 -0500

    segundo

--- controlnotas/app.py
+++ controlnotas/app.py
@@ -283,12 +283,6 @@ def descargar_rechazados():
 
 
 if __name__ == "__main__":
-    try:
-        conexion = conectar()
-        print("todo funciona le peluche mi pez")
-        conexion.close()
-    except Exception as e:
-        print("Error al conectar:", e)
-
-    print("corriendo en http://127.0.0.1:5000/")
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
